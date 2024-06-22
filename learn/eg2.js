723976f7181e062ed6cfca0f7830552174affa88
Aayush Bhatt <99087961+aayushbhatt044@users.noreply.github.com>
2024-06-22 07:59:31 +0000

    Container Config Done

---
+++ learn/eg2.js
@@ -0,0 +1,14 @@
+const rollNumber;
+rollNumber=101;
+var name="Aayush";
+let age=20;
+console.log(rollNumber);
+console.log(name);
+console.log(age);
+// let's change the variable values & see what happened
+rollNumber=102;
+name="Aayush Bhatt";
+age=23;
+console.log(rollNumber);
+console.log(name);
+console.log(age);
